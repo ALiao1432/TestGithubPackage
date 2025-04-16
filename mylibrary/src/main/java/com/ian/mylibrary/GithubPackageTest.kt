1ffd91268632070ed862520e43c9d820e4755edb
Ian Liao <ian@redso.com.hk>
2025-04-16 17:47:02 +0800

    chore: initial project

---
+++ mylibrary/src/main/java/com/ian/mylibrary/GithubPackageTest.kt
@@ -0,0 +1,9 @@
+package com.ian.mylibrary
+
+class GithubPackageTest {
+    fun test() {
+        // This is a test function to check if the package is working correctly
+        // You can add your test cases here
+        println("Github Package Test")
+    }
+}
